c0e50822268be81ffe290b19aec90e474402bf04
niyioo <niyiolagbegi@gmail.com>
2024-01-17 14:15:20 +0100

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Create a function SafeDiv
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10, 4)
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END //
+
+DELIMITER ;
