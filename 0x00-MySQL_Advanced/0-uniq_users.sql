93bbf99ea1a383038b9731198be31cc27ed5144a
niyioo <niyiolagbegi@gmail.com>
2024-01-17 12:47:39 +0100

    tasks

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create the 'users' table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
