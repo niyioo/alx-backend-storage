bee62ed063eeef7b56819cee44cda70e8ea2c7c8
niyioo <niyiolagbegi@gmail.com>
2024-01-17 13:19:29 +0100

    fx

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rank country origins of bands by the number of non-unique fans
-SELECT origin, COUNT(fan_id) AS nb_fans
+SELECT DISTINCT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
