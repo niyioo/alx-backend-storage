d9110fc67cd31cdfc285e07e327d00a600fc7e5b
niyioo <niyiolagbegi@gmail.com>
2024-01-18 13:27:42 +0100

    fix 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,5 @@
 -- List Glam rock bands ranked by longevity
-SELECT band_name, (IFNULL(split, 2022) - formed)
-AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
