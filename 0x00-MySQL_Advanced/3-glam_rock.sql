795397387f243b76e6d80759917df11654d4d731
niyioo <niyiolagbegi@gmail.com>
2024-01-18 13:10:47 +0100

    fix 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 SELECT band_name,
        IF(SUBSTRING_INDEX(formed, '-', -1) != '0', 2022 - CAST(SUBSTRING_INDEX(formed, '-', -1) AS SIGNED), 0) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', style) > 0
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
