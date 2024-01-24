040c64e8497fecb388e20cfc8857144f56fb7d07
niyioo <niyiolagbegi@gmail.com>
2024-01-24 13:14:53 +0100

    ddd

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Cache module
+create a connection with redis database
 """
 import redis
 from uuid import uuid4
@@ -8,84 +8,81 @@ from typing import Callable, Optional, Union
 from functools import wraps
 
 
-def record_user_actions(method: Callable) -> Callable:
-    """Decorator to log user actions"""
+def call_history(method: Callable) -> Callable:
+    """ memorize user actions"""
     method_key = method.__qualname__
-    inputs_key = method_key + ':inputs'
-    outputs_key = method_key + ':outputs'
+    inputs = method_key + ':inputs'
+    outputs = method_key + ':outputs'
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """Wrapped function to record user actions"""
-        self._redis.rpush(inputs_key, str(args))
-        result = method(self, *args, **kwargs)
-        self._redis.rpush(outputs_key, str(result))
-        return result
-
+    def wrapper(self, *args, **kwds):
+        """ function  wrapped """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
     return wrapper
 
 
-def count_method_calls(method: Callable) -> Callable:
-    """Decorator to count method calls"""
+def count_calls(method: Callable) -> Callable:
+    """ counts method call """
     method_key = method.__qualname__
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """Wrapped function to count method calls"""
+    def wrapper(self, *args, **kwds):
+        """wrapped function """
         self._redis.incr(method_key)
-        return method(self, *args, **kwargs)
-
+        return method(self, *args, **kwds)
     return wrapper
 
 
-def display_call_history(method: Callable):
-    """Function to display the call history of a method"""
+def replay(method: Callable):
+    """ display the history call """
     method_key = method.__qualname__
-    inputs_key = method_key + ":inputs"
-    outputs_key = method_key + ":outputs"
-    redis_instance = method.__self__._redis
-    call_count = redis_instance.get(method_key).decode("utf-8")
-    print("{} was called {} times:".format(method_key, call_count))
-    input_list = redis_instance.lrange(inputs_key, 0, -1)
-    output_list = redis_instance.lrange(outputs_key, 0, -1)
-    all_data = list(zip(input_list, output_list))
-    for input_data, output_data in all_data:
-        input_str, output_str = input_data.decode(
-            "utf-8"), output_data.decode("utf-8")
-        print("{}(*{}) -> {}".format(method_key, input_str, output_str))
+    inputs = method_key + ":inputs"
+    outputs = method_key + ":outputs"
+    redis = method.__self__._redis
+    count = redis.get(method_key).decode("utf-8")
+    print("{} was called {} times:".format(method_key, count))
+    ListInput = redis.lrange(inputs, 0, -1)
+    ListOutput = redis.lrange(outputs, 0, -1)
+    allData = list(zip(ListInput, ListOutput))
+    for key, data in allData:
+        attr, data = key.decode("utf-8"), data.decode("utf-8")
+        print("{}(*{}) -> {}".format(method_key, attr, data))
 
 
 class Cache:
-    """Class to store information"""
+    """
+    store informations
+    """
 
     def __init__(self):
-        """Constructor to initialize the instance"""
+        """construct the instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @record_user_actions
-    @count_method_calls
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store new data and return a new UUID"""
+        """ store new data and return a new uuid"""
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
 
-    def get(
-            self,
+    def get(self,
             key: str,
-            fn: Optional[Callable] = None
-            ) -> Union[str, bytes, int, float]:
-        """Get the element and return the decoded version"""
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get the element and return the decoded version"""
         data = self._redis.get(key)
-        if fn is not None:
+        if (fn is not None):
             return fn(data)
         return data
 
     def get_str(self, data: str) -> str:
-        """Return the decoded byte in string"""
+        """ return the decoded byte in string """
         return data.decode('utf-8')
 
     def get_int(self, data: str) -> int:
-        """Return the decoded byte in integer"""
+        """ return the decoded byte in integer """
         return int(data)
