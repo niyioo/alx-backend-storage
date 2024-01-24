b0b8f1e725a06077f3914cf5e297adc438f7533b
niyioo <niyiolagbegi@gmail.com>
2024-01-24 12:02:51 +0100

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Cache module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 
 class Cache:
@@ -19,7 +19,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis and return the key
@@ -27,3 +26,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis using key and apply optional conversion function fn
+        """
+        result = self._redis.get(key)
+        if result is not None and fn is not None:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve string data from Redis using key
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve integer data from Redis using key
+        """
+        return self.get(key, fn=int)
